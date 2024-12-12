2c7591ac79afc0869db4379d5e6153a850bc6111
Muhammad Umar Mansyur <umar.ovie@gmail.com>
2024-12-12 16:11:39 +0700

    Remove unused client directive and clean up imports in StatusFilter component

--- src/app/admin/pabrik/StatusFilter.tsx
+++ src/app/admin/pabrik/StatusFilter.tsx
@@ -1,7 +1,6 @@
-// "use client";
+
 import React from 'react';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
-
 interface StatusFilterProps {
   selectedStatus: string;
   onStatusChange: (status: string) => void;
